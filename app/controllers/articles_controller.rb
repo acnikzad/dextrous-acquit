63bbadd4735b89d3bdfd71fb7ff0542a7f3436f0
Aria Nikzad <a.c.nikzad@gmail.com>
2022-10-13 15:05:27 -0700

    added CRUD to article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,32 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+    render json: @articles
+  end 
+
+  def show
+    @article = Article.find_by(id: params[:id])
+    render json: @article
+  end
+
+  def create
+    @article = Article.create(
+      title: params[:title],
+      content: params[:content],
+      author: params[:author],
+      category: params[:category],
+      published_at: params[:published_at]
+      )
+    @article.save
+    render json: @article
+  end
+
+  def update
+    render :error
+  end
+
+  def destroy
+    render :error
+  end
 end
