89757ff93d64282ed9f4900a24ccbb72815e7958
Aria Nikzad <a.c.nikzad@gmail.com>
2022-10-13 15:01:59 -0700

    added routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    get "/articles" => "articles#index"
+    get "/articles/:id" => "articles#show"
+    post "/articles" => "articles#create"
+    patch "/articles/:id" => "articles#update"
+    delete "/articles/:id" => "articles#destroy"
+    
 end
