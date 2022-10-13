0ae3b89c19cea1abad8acd42d61909e76ccb7721
Aria Nikzad <a.c.nikzad@gmail.com>
2022-10-13 15:19:43 -0700

    added PUT route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     get "/articles" => "articles#index"
     get "/articles/:id" => "articles#show"
     post "/articles" => "articles#create"
+    put "/articles" => "articles#update"
     patch "/articles/:id" => "articles#update"
     delete "/articles/:id" => "articles#destroy"
-    
+
 end
