d37ea63c5e442cb5db052f823c086a0ed6c87910
Aria Nikzad <a.c.nikzad@gmail.com>
2022-10-13 15:18:31 -0700

    added validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+
+  validates :title, presence: true
+  validates :content, presence: true, length: {maximum: 40, too_long: "That is too many characters"}
+  validates :author, presence: true
+  validates :category, presence: true
+  validates :published_at, presence: true
+
 end
